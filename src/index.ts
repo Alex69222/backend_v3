25180f38daf968cbc81c060c16faa05e8d334ed6
Alex <bbqsauce108@gmail.com>
2023-09-05 13:24:17 +0400

    feat: add post/put/delete for products

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, {Request, Response} from 'express'
+
 const app = express()
 const port = 3000
 
-const products = [{title: 'tomato'}, {title: 'orange'}]
-const addresses = [{value: 'Gorgiladze 14'}, {title: 'Chaikovskogo 191'}]
+let products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
+let addresses = [{id: 1, value: 'Gorgiladze 14'}, {id: 2, title: 'Chaikovskogo 191'}]
+app.use(express.json())
 app.get('/', (req: Request, res: Response) => {
     let string =
         '<p>Hello Best Backend developer Ever!</p>' +
@@ -12,14 +14,54 @@ app.get('/', (req: Request, res: Response) => {
         '<p>You know you can do it!</p>'
     res.send(string)
 })
-app.get('/products', (req: Request, res: Response ) => {
-    res.send(products)
+app.get('/products', (req: Request, res: Response) => {
+    const queryTitle = req.query.title
+    if (queryTitle && typeof queryTitle === "string") {
+        const filteredProducts = products.filter(p => p.title.includes(queryTitle))
+        if (!filteredProducts.length) return res.sendStatus(404)
+        res.send(filteredProducts)
+    } else {
+        res.send(products)
+    }
+})
+app.get('/products/:id', (req: Request, res: Response) => {
+    const product = products.find(p => p.id === +req.params.id)
+    if (!product) return res.sendStatus(404)
+    res.send(product)
 })
 
-app.get('/addresses', (req: Request, res: Response ) => {
+app.post('/products', (req: Request, res: Response) => {
+    const newProduct = {
+        id: +(new Date),
+        title: req.body.title
+    }
+    products.push(newProduct)
+    res.status(201).send(newProduct)
+})
+app.put('/products/:id', (req: Request, res: Response) => {
+    const product = products.find(p => p.id === +req.params.id)
+    if (!product) return res.sendStatus(404)
+    product.title = req.body.title;
+    res.send(product)
+})
+app.delete('/products/:id', (req: Request, res: Response) => {
+
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            products.splice(i, 1)
+            return res.sendStatus(204)
+        }
+    }
+    res.sendStatus(404)
+})
+app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses)
 })
-
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    const address = addresses.find(a => a.id === +req.params.id)
+    if (!address) return res.sendStatus(404)
+    res.send(address)
+})
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
